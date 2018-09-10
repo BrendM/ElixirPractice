be9efaff912be8b58ddd6fdfb2c7aae1f977377c
bmangan <b.mangan@pilz.ie>
2018-09-10 09:30:22 +0100

    added more practice code

--- elixir_practice/lib/practice.ex
+++ elixir_practice/lib/practice.ex
@@ -9,8 +9,7 @@ defmodule Practice do
   def elementAt(list, pos) do
     elementAt(list, pos, 0)
   end
-
-  defp elementAt([head|tail], pos, current) do
+  defp elementAt([head | tail], pos, current) do
     if pos == current do
       head
     else
@@ -21,11 +20,39 @@ defmodule Practice do
   def myReverse(list) do
     myReverse(list, [])
   end
-
-  defp myReverse([head|tail], newList) do
+  defp myReverse([head | tail], newList) do
     myReverse(tail, List.flatten([head, newList]))
   end
   defp myReverse([], newList) do
     newList
+  end
+
+  def flatten(list) do
+    flatten(list, [])
+  end
+  defp flatten([], flatList) do
+    flatList
+  end
+  defp flatten([[head | tail] | tailOuter], newList) do
+    flatten(tailOuter, newList ++ [head] ++ flatten(tail))
+  end
+  defp flatten([head | tail], newList) do
+    flatten(tail, newList ++ [head])
+  end
+
+  def compress([head | tail]) do
+    puts(head)
+    puts(tail)
+    compress([head | tail], head, [])
+  end
+  defp compress([head, tail], target, compressedList) do
+    if head == target do
+      compress(tail, target, compressedList)
+    else
+      compress(tail, head, compressedList ++ [head])
+    end
+  end
+  defp compress([], nil, compressedList) do
+    compressedList
   end
 end
--- elixir_practice/test/elixir_practice_test.exs
+++ elixir_practice/test/elixir_practice_test.exs
@@ -1,22 +1,36 @@
 defmodule ElixirPracticeTest do
   use ExUnit.Case
+  import Practice
   doctest ElixirPractice
 
   test "last element of a list" do
-    assert Practice.last([1]) == 1
-    assert Practice.last([1, 2, 3]) == 3
-    assert Practice.last(["hey", 2, "ya"]) == "ya"
+    assert last([1]) == 1
+    assert last([1, 2, 3]) == 3
+    assert last(["hey", 2, "ya"]) == "ya"
   end
 
   test "get elements at a position in a list" do
-    assert Practice.elementAt([1], 0) == 1
-    assert Practice.elementAt([1, 2], 1) == 2
-    assert Practice.elementAt([1, 2, "yea"], 2) == "yea"
+    assert elementAt([1], 0) == 1
+    assert elementAt([1, 2], 1) == 2
+    assert elementAt([1, 2, "yea"], 2) == "yea"
   end
 
   test "reversing a list" do
-    assert Practice.myReverse([1,2,3]) == [3,2,1]
-    assert Practice.myReverse([1]) == [1]
-    assert Practice.myReverse([]) == []
+    assert myReverse([1, 2, 3]) == [3, 2, 1]
+    assert myReverse([1]) == [1]
+    assert myReverse([]) == []
+  end
+
+  test "list flattening" do
+    assert flatten([1, 2, 3]) == [1, 2, 3]
+    assert flatten([1, 2, [3]]) == [1, 2, 3]
+    assert flatten([1, 2, [3, 4]]) == [1, 2, 3, 4]
+    assert flatten([1, 2, [3, 4]]) == List.flatten([1, 2, [3, 4]])
+  end
+
+  test "list compression" do
+    assert compress([1,1]) == [1]
+    assert compress(["a", "b", "c"]) == ["a", "b", "c"]
+    assert compress(["a", "a", "c"]) == ["a", "c"]
   end
 end
